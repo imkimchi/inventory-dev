0b6bce23ce76d419eb02e7f785f9eaa3226dca58
imkimchi <wbvcos@gmail.com>
2017-10-11 06:23:50 +0900

    fixed image path issue

--- src/util/formidable.js
+++ src/util/formidable.js
@@ -8,7 +8,7 @@ form.encoding = "utf-8"
 form.on("fileBegin", function(name, file) {
     let regex = /[^.]*/
     let fileName = file.name.replace(regex, shortid())
-    file.path = path.join(__dirname + '/../views/images/uploads', fileName)
+    file.path = path.join(__dirname + '/../../views/images/uploads', fileName)
 })
 
 export default form
